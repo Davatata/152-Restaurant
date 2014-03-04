41054a94851c6e78f8b26fe4b854991b6352d85e
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2014-03-03 16:40:19 -0800

    User update
    User activity updated.

--- TableNow/src/com/tablenow/User.java
+++ TableNow/src/com/tablenow/User.java
@@ -1,12 +1,14 @@
 package com.tablenow;
 
-import android.os.Bundle;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
-import android.annotation.TargetApi;
-import android.os.Build;
+import android.view.View;
 
 public class User extends Activity {
 
@@ -26,6 +28,10 @@ public class User extends Activity {
 		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
+	}
+	
+	public void to_search(View V){
+		startActivity(new Intent(User.this, UserSearch.class));
 	}
 
 	@Override
