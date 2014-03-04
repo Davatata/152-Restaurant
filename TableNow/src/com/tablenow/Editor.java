a6584a2613ca49cb43e2810cb7b61ae949d0ff58
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2014-03-03 16:18:22 -0800

    update
    restaurant update

--- TableNow/src/com/tablenow/Editor.java
+++ TableNow/src/com/tablenow/Editor.java
@@ -1,21 +1,19 @@
 package com.tablenow;
 
-import android.annotation.TargetApi;
-import android.app.Activity;
-import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.NavUtils;
+import android.app.Activity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
+import android.support.v4.app.NavUtils;
+import android.annotation.TargetApi;
+import android.os.Build;
 
-public class Restaurant extends Activity {
+public class Editor extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_restaurant);
+		setContentView(R.layout.activity_editor);
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -29,15 +27,11 @@ public class Restaurant extends Activity {
 			getActionBar().setDisplayHomeAsUpEnabled(true);
 		}
 	}
-	
-	public void to_editor(View v){
-		startActivity(new Intent(Restaurant.this, Editor.class));
-	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.restaurant, menu);
+		getMenuInflater().inflate(R.menu.editor, menu);
 		return true;
 	}
 
