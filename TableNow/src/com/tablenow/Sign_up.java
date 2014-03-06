e24c0f6bf942dd7caca3113f7fb047057a58c72a
Ying Vang <yingvang30@gmail.com>
2014-03-06 13:36:09 -0800

    The Sign_up Class functionality Update 1

--- TableNow/src/com/tablenow/Sign_up.java
+++ TableNow/src/com/tablenow/Sign_up.java
@@ -1,19 +1,69 @@
 package com.tablenow;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.support.v4.app.NavUtils;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.os.Build;
 
-public class Sign_up extends Activity {
 
+public class Sign_up extends Activity {
+	// Progress Dialog
+    private ProgressDialog pDialog;
+ 
+    JSONParser jsonParser = new JSONParser();
+    EditText inputFirstName;
+    EditText inputLastName;
+    EditText inputEmail;
+    EditText inputPassowrd;
+    
+	// url to create new product
+    private static String url_create_User = "http://api.androidhive.info/android_connect/create_product.php";
+ 
+    // JSON Node names
+    private static final String TAG_SUCCESS = "success";
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_sign_up);
+		
+		// Edit Text
+		inputFirstName = (EditText) findViewById(R.id.first_name_editText);
+		inputLastName = (EditText) findViewById(R.id.last_name_editText);
+		inputEmail = (EditText) findViewById(R.id.email_editText);
+		inputPassowrd = (EditText) findViewById(R.id.password_editText);
+ 
+        // Create button
+        Button joinNowBtn = (Button) findViewById(R.id.join_button);
+ 
+        // button click event
+        joinNowBtn.setOnClickListener(new View.OnClickListener() {
+ 
+            @Override
+            public void onClick(View view) {
+                // creating new product in background thread
+                new CreateNewUser().execute();
+            }
+        });
+		
 		// Show the Up button in the action bar.
 		setupActionBar();
 	}
@@ -51,5 +101,79 @@ public class Sign_up extends Activity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-
+	
+	/**
+     * Background Async Task to Add new user
+     * */
+    class CreateNewUser extends AsyncTask<String, String, String> {
+ 
+        /**
+         * Before starting background thread Show Progress Dialog
+         * */
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            pDialog = new ProgressDialog(Sign_up.this);
+            pDialog.setMessage("Creating Product..");
+            pDialog.setIndeterminate(false);
+            pDialog.setCancelable(true);
+            pDialog.show();
+        }
+ 
+        /**
+         * Adding new user
+         * */
+        protected String doInBackground(String... args) {
+            String firstName = inputFirstName.getText().toString();
+            String lastName = inputLastName.getText().toString();
+            String userEmail = inputEmail.getText().toString();
+            String userPassword = inputPassowrd.getText().toString();
+ 
+            // Building Parameters
+            List<NameValuePair> params = new ArrayList<NameValuePair>();
+            params.add(new BasicNameValuePair("FirstName", firstName));
+            params.add(new BasicNameValuePair("LastName", lastName));
+            params.add(new BasicNameValuePair("UserEmail", userEmail));
+            params.add(new BasicNameValuePair("UserPassowrd", userPassword));
+ 
+            // getting JSON Object
+            // Note that create product url accepts POST method
+            JSONObject json = jsonParser.makeHttpRequest(url_create_User,
+                    "POST", params);
+ 
+            // check log cat for response
+            Log.d("Create Response", json.toString());
+ 
+            // check for success tag
+            try {
+                int success = json.getInt(TAG_SUCCESS);
+ 
+                if (success == 1) {
+                    // successfully created user
+                    // notify previous activity by sending code 100
+                    Intent i = getIntent();
+                    // send result code 100 to notify about product saving
+                    setResult(100, i);
+                    finish();
+ 
+                    // closing this screen
+                    finish();
+                } else {
+                    // failed to create user
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+ 
+            return null;
+        }
+        
+        /**
+         * After completing background task Dismiss the progress dialog
+         * **/
+        protected void onPostExecute(String file_url) {
+            // dismiss the dialog once done
+            pDialog.dismiss();
+        }
+    }
 }
