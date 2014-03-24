fc3a7592303d6135789ab35130f9d45938e17faf
JohnGarcia <JohnGarcia05@mail.fresnostate.edu>
2014-03-24 15:24:25 -0700

    strings update

--- TableNow/gen/com/tablenow/R.java
+++ TableNow/gen/com/tablenow/R.java
@@ -36,8 +36,9 @@ public final class R {
         public static final int TextView02=0x7f0a0014;
         public static final int TextView03=0x7f0a0013;
         public static final int User=0x7f0a0005;
-        public static final int action_settings=0x7f0a001a;
-        public static final int checkBox1=0x7f0a0004;
+        public static final int action_settings=0x7f0a001b;
+        public static final int checkBox_res=0x7f0a0016;
+        public static final int checkBox_user=0x7f0a0004;
         public static final int default_layout=0x7f0a000d;
         public static final int editorButton=0x7f0a000a;
         public static final int email_editText=0x7f0a0011;
@@ -50,11 +51,11 @@ public final class R {
         public static final int passwordEditText=0x7f0a0002;
         public static final int passwordTextView=0x7f0a0000;
         public static final int password_editText=0x7f0a0012;
-        public static final int search_button=0x7f0a0019;
-        public static final int search_editText=0x7f0a0017;
-        public static final int search_textView=0x7f0a0018;
+        public static final int search_button=0x7f0a001a;
+        public static final int search_editText=0x7f0a0018;
+        public static final int search_textView=0x7f0a0019;
         public static final int settings_spinner_r=0x7f0a000c;
-        public static final int settings_spinner_u=0x7f0a0016;
+        public static final int settings_spinner_u=0x7f0a0017;
         public static final int signUpButton=0x7f0a0007;
         public static final int usernameEditText=0x7f0a0003;
         public static final int usernameTextView=0x7f0a0001;
@@ -83,58 +84,60 @@ public final class R {
         public static final int user_update=0x7f090009;
     }
     public static final class string {
-        public static final int Editor=0x7f060011;
+        public static final int Editor=0x7f060013;
         public static final int Email=0x7f060005;
         public static final int First_Name=0x7f060003;
         public static final int Join=0x7f060007;
         public static final int Last_Name=0x7f060004;
         public static final int Password=0x7f060006;
-        public static final int Search_button=0x7f060012;
-        public static final int Search_text=0x7f060013;
+        public static final int Search_button=0x7f060014;
+        public static final int Search_text=0x7f060015;
         public static final int action_settings=0x7f060001;
         public static final int app_name=0x7f060000;
-        public static final int hello_world=0x7f060019;
+        public static final int cancel=0x7f060009;
+        public static final int hello_world=0x7f06001b;
         public static final int log_in=0x7f060008;
-        public static final int pref_default_display_name=0x7f060023;
-        public static final int pref_description_social_recommendations=0x7f060021;
+        public static final int pref_default_display_name=0x7f060025;
+        public static final int pref_description_social_recommendations=0x7f060023;
         /**  Example settings for Data & Sync 
          */
-        public static final int pref_header_data_sync=0x7f060025;
+        public static final int pref_header_data_sync=0x7f060027;
         /**  Strings related to Settings 
  Example General settings 
          */
-        public static final int pref_header_general=0x7f06001f;
+        public static final int pref_header_general=0x7f060021;
         /**  Example settings for Notifications 
          */
-        public static final int pref_header_notifications=0x7f060028;
-        public static final int pref_ringtone_silent=0x7f06002b;
-        public static final int pref_title_add_friends_to_messages=0x7f060024;
-        public static final int pref_title_display_name=0x7f060022;
-        public static final int pref_title_new_message_notifications=0x7f060029;
-        public static final int pref_title_ringtone=0x7f06002a;
-        public static final int pref_title_social_recommendations=0x7f060020;
-        public static final int pref_title_sync_frequency=0x7f060026;
-        public static final int pref_title_system_sync_settings=0x7f060027;
-        public static final int pref_title_vibrate=0x7f06002c;
-        public static final int remember_me=0x7f06000e;
-        public static final int restaurant=0x7f06000f;
+        public static final int pref_header_notifications=0x7f06002a;
+        public static final int pref_ringtone_silent=0x7f06002d;
+        public static final int pref_title_add_friends_to_messages=0x7f060026;
+        public static final int pref_title_display_name=0x7f060024;
+        public static final int pref_title_new_message_notifications=0x7f06002b;
+        public static final int pref_title_ringtone=0x7f06002c;
+        public static final int pref_title_social_recommendations=0x7f060022;
+        public static final int pref_title_sync_frequency=0x7f060028;
+        public static final int pref_title_system_sync_settings=0x7f060029;
+        public static final int pref_title_vibrate=0x7f06002e;
+        public static final int remember_me=0x7f060010;
+        public static final int restaurant=0x7f060011;
+        public static final int save=0x7f06000a;
         public static final int sign_up=0x7f060002;
-        public static final int title_activity_editor=0x7f06001a;
-        public static final int title_activity_restaurant=0x7f060014;
-        public static final int title_activity_restaurant_update=0x7f06001c;
-        public static final int title_activity_sign_up=0x7f060018;
-        public static final int title_activity_user=0x7f060015;
-        public static final int title_activity_user_layout=0x7f060017;
-        public static final int title_activity_user_results=0x7f06001b;
-        public static final int title_activity_user_search=0x7f060016;
-        public static final int title_activity_user_settings=0x7f06001e;
-        public static final int title_activity_user_update=0x7f06001d;
-        public static final int user=0x7f060010;
-        public static final int view_email=0x7f06000d;
-        public static final int view_firstname=0x7f060009;
-        public static final int view_lastname=0x7f06000a;
-        public static final int view_password=0x7f06000b;
-        public static final int view_username=0x7f06000c;
+        public static final int title_activity_editor=0x7f06001c;
+        public static final int title_activity_restaurant=0x7f060016;
+        public static final int title_activity_restaurant_update=0x7f06001e;
+        public static final int title_activity_sign_up=0x7f06001a;
+        public static final int title_activity_user=0x7f060017;
+        public static final int title_activity_user_layout=0x7f060019;
+        public static final int title_activity_user_results=0x7f06001d;
+        public static final int title_activity_user_search=0x7f060018;
+        public static final int title_activity_user_settings=0x7f060020;
+        public static final int title_activity_user_update=0x7f06001f;
+        public static final int user=0x7f060012;
+        public static final int view_email=0x7f06000f;
+        public static final int view_firstname=0x7f06000b;
+        public static final int view_lastname=0x7f06000c;
+        public static final int view_password=0x7f06000d;
+        public static final int view_username=0x7f06000e;
     }
     public static final class style {
         /** 
