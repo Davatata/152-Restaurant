d06db743817dee5dc71b9b1783c0c122561a8191
David <davatata@gmail.com>
2014-04-25 01:40:17 -0700

    php config changes

--- TableNow/src/com/tablenow/db_config.php
+++ TableNow/src/com/tablenow/db_config.php
@@ -3,8 +3,8 @@
 
 define('DB_USER',"teamDave");
 define('DB_PASSWORD',"csci152");
-//define ('DB_DATABASE',""); //database name
-define ('DB_SERVER', "98.239.80.190");
+define ('DB_DATABASE',"restaurant"); //database name
+define ('DB_SERVER', "50.161.166.152");
 
 ?>
 
