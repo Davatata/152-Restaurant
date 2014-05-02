ac240da1c012f3a3f7df99917020c92597de8a53
Ying Vang <yingvang30@gmail.com>
2014-05-02 15:50:00 -0700

    Finished Setting Up Robolectrics for Testing.

---
+++ TableNow/test/MainActivityTest.java
@@ -0,0 +1,28 @@
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.hamcrest.Matcher;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+
+import android.R;
+import android.R.string;
+
+import com.tablenow.MainActivity;
+
+
+
+@RunWith(RobolectricTestRunner.class)
+public class MainActivityTest {
+	
+	@Test
+	public void test() throws Exception{
+		
+	}
+
+	private void assertThat(string hello, Matcher<String> equalTo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
