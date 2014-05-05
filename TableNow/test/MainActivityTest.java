2f3ada1ec6ccea76b84ad94e5a5a51c32d2c349a
Ying Vang <yingvang30@gmail.com>
2014-05-04 23:39:47 -0700

    MainActivityTest.java Successful!

--- TableNow/test/MainActivityTest.java
+++ TableNow/test/MainActivityTest.java
@@ -9,7 +9,6 @@ import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowIntent;
 import static org.junit.Assert.assertThat;
 //import static org.robolectric.Robolectric.shadowOf;
-
 import android.content.Intent;
 import android.widget.Button;
 
@@ -21,29 +20,18 @@ import com.tablenow.Sign_up;
 
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
-	
-	private MainActivity activity;
-	private Button pressMeButton;
-	
-	@Before
-	public void setUp() {
-        activity = new MainActivity();
-        activity.onCreate(null);
 
-        pressMeButton = (Button) activity.findViewById(R.id.signUpButton);
-  
-    }
-	
-	@Test
-	 public void should_start_result_activity_when_pluss_button_is_clicked() {
-		pressMeButton.performClick();
+	  @Test
+	  public void clickingButton_shouldChangeResultsViewText() throws Exception {
+	    MainActivity activity = Robolectric.buildActivity(MainActivity.class).create().get();
 
-        ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+	    Button pressMeButton = (Button) activity.findViewById(R.id.signUpButton);
+
+	    pressMeButton.performClick();
+	    
+	    ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
         Intent startedIntent = shadowActivity.getNextStartedActivity();
         ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
-
-        assertThat(shadowIntent.getComponent().getClassName(), equalTo(Sign_up.class.getName()));
-    }
-
-
+	    assertThat(shadowIntent.getComponent().getClassName(), equalTo(Sign_up.class.getName()));
+	  }
 }
