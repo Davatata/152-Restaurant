9d817e6ab9c9a9f5a8d90c6268cf8c169595bcba
Ying Vang <yingvang30@gmail.com>
2014-05-16 21:36:42 -0700

    Two more test

--- TableNow/test/Sign_upTest.java
+++ TableNow/test/Sign_upTest.java
@@ -6,6 +6,7 @@ import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowEnvironment;
 import org.robolectric.shadows.ShadowIntent;
 
 import static org.junit.Assert.*;
@@ -20,6 +21,7 @@ import android.widget.CheckBox;
 import com.tablenow.Editor;
 import com.tablenow.MainActivity;
 import com.tablenow.R;
+import com.tablenow.R.menu;
 import com.tablenow.Sign_up;
 import com.tablenow.User;
 
@@ -28,8 +30,27 @@ import com.tablenow.User;
 @RunWith(RobolectricTestRunner.class)
 public class Sign_upTest {
 	
+	@SuppressWarnings("null")
 	@Test
 	  public void onCreateOptionMenuTest() throws Exception {
+	    Sign_up activity = Robolectric.buildActivity(Sign_up.class).create().get();
+	    Menu menu=null;
+	    ((ShadowActivity) menu).getMenuInflater().inflate(R.menu.sign_up, menu);
+	    assertThat(menu, equalTo(null));
+	  }
+	
+	@Test
+	  public void UserCheckTest() throws Exception {
+	    Sign_up activity = Robolectric.buildActivity(Sign_up.class).create().get();
+	    View view = null;
+	    activity.check_res(view);
+	    @SuppressWarnings("null")
+		boolean cs = ((CheckBox) view).isChecked();
+	    assertEquals(false, cs);
+	  }
+	
+	@Test
+	  public void RESCheckTest() throws Exception {
 	    Sign_up activity = Robolectric.buildActivity(Sign_up.class).create().get();
 	    View view = null;
 	    activity.check_res(view);
