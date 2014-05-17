e45999048410b526df540f8c159175b7db304dce
Ying Vang <yingvang30@gmail.com>
2014-05-16 22:27:44 -0700

    Test case "onCreateOptionMenuTest" "onOptionsItemSelected"

--- TableNow/test/EditorTest.java
+++ TableNow/test/EditorTest.java
@@ -7,12 +7,15 @@ import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
 import org.robolectric.shadows.ShadowActivity;
 import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.tester.android.view.TestMenuItem;
+
 import static org.junit.Assert.assertThat;
 //import static org.robolectric.Robolectric.shadowOf;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.KeyEvent;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebView;
 import android.widget.Button;
 
@@ -27,10 +30,13 @@ import com.tablenow.User;
 @RunWith(RobolectricTestRunner.class)
 public class EditorTest {
 	
+	@SuppressWarnings("null")
 	@Test
 	  public void onCreatTest() throws Exception {
-	    Editor activity = Robolectric.buildActivity(Editor.class).create().get();
-	    Bundle savedInstanceState = null;
+	    @SuppressWarnings("unused")
+		Editor activity = Robolectric.buildActivity(Editor.class).create().get();
+	    @SuppressWarnings("unused")
+		Bundle savedInstanceState = null;
 		String url="http://50.161.166.152/restaurant/pages/restaurant.html";
 		WebView wView = null;
 		wView.loadUrl(url);
@@ -46,6 +52,33 @@ public class EditorTest {
 	    assertThat(keyE, equalTo(null));  
 		
 	}
-		
+	
+	@Test
+	  public void onOptionsItemSelectedTest() throws Exception {
+		Editor activity=Robolectric.buildActivity(Editor.class).create().get();
+	    MenuItem item = new TestMenuItem() {
+		  public int getItemId() {
+		    return R.id.action_sign_out;
+		  }
+		};
+
+		activity.onOptionsItemSelected(item);
+
+		ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+		Intent startedIntent = shadowActivity.getNextStartedActivity();
+		ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
+
+		assertThat(shadowIntent.getComponent().getClassName(), equalTo(Sign_up.class.getName()));
+	  }
+	
+	@SuppressWarnings("null")
+	@Test
+	  public void onCreateOptionMenuTest() throws Exception {
+	    @SuppressWarnings("unused")
+		Sign_up activity = Robolectric.buildActivity(Sign_up.class).create().get();
+	    Menu menu=null;
+	    ((ShadowActivity) menu).getMenuInflater().inflate(R.menu.editor, menu);
+	    assertThat(menu, equalTo(null));
+	  }
 	
 }
