43fb222215d149bd858ebc056af7b32bd1a0e71c
Ying Vang <yingvang30@gmail.com>
2014-05-16 23:14:17 -0700

    "UserLayoutTest" test cases

---
+++ TableNow/test/UserLayoutTest.java
@@ -0,0 +1,83 @@
+import static org.hamcrest.CoreMatchers.equalTo;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowActivity;
+import org.robolectric.shadows.ShadowIntent;
+import org.robolectric.tester.android.view.TestMenuItem;
+
+import static org.junit.Assert.assertThat;
+//import static org.robolectric.Robolectric.shadowOf;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.webkit.WebView;
+import android.widget.Button;
+
+import com.tablenow.Editor;
+import com.tablenow.MainActivity;
+import com.tablenow.R;
+import com.tablenow.Sign_up;
+import com.tablenow.User;
+import com.tablenow.UserLayout;
+
+
+
+@RunWith(RobolectricTestRunner.class)
+public class UserLayoutTest {
+	
+	@SuppressWarnings("null")
+	@Test
+	  public void onCreatTest() throws Exception {
+		UserLayout activity = Robolectric.buildActivity(UserLayout.class).create().get();
+		Bundle savedInstanceState = null;
+		String url="http://50.161.166.152/restaurant/pages/restaurant.html";
+		WebView wView = null;
+		wView.loadUrl(url);
+	    assertThat(true, equalTo(true));  
+	  }
+	
+	@Test
+	  public void onKeyDownTest() throws Exception {
+		UserLayout activity = Robolectric.buildActivity(UserLayout.class).create().get();
+		int key=4534;
+		KeyEvent keyE = null;
+		activity.onKeyDown(key, keyE);
+	    assertThat(keyE, equalTo(null));  
+		
+	}
+	
+	@Test
+	  public void onOptionsItemSelectedTest() throws Exception {
+		UserLayout activity=Robolectric.buildActivity(UserLayout.class).create().get();
+	    MenuItem item = new TestMenuItem() {
+		  public int getItemId() {
+		    return R.id.action_settings;
+		  }
+		};
+
+		activity.onOptionsItemSelected(item);
+
+		ShadowActivity shadowActivity = Robolectric.shadowOf(activity);
+		Intent startedIntent = shadowActivity.getNextStartedActivity();
+		ShadowIntent shadowIntent = Robolectric.shadowOf(startedIntent);
+
+		assertThat(shadowIntent.getComponent().getClassName(), equalTo(UserLayout.class.getName()));
+	  }
+	
+	@SuppressWarnings("null")
+	@Test
+	  public void onCreateOptionMenuTest() throws Exception {
+	    @SuppressWarnings("unused")
+		Sign_up activity = Robolectric.buildActivity(Sign_up.class).create().get();
+	    Menu menu=null;
+	    ((ShadowActivity) menu).getMenuInflater().inflate(R.menu.editor, menu);
+	    assertThat(menu, equalTo(null));
+	  }
+	
+}
